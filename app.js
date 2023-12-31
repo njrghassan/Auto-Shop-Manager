fdd2317d1e144f6220f97023e3de9e54b2b13947
Ghassan Alnajjar <ghassan.f.najjar@gmail.com>
2023-12-30 20:48:28 -0700

    Values are fix. Still trying to fix the total cost

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const { JSDOM } = require("jsdom");
-const { window } = new JSDOM("");
-const $ = require("jquery")(window);
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
 const totalCost = require(__dirname + "/public/scripts/billProduct.js");
 
 app.use(express.static("public"), bodyParser.urlencoded({ extended: true }));
 
-// date and time
+//date and time
 const curDate = require(__dirname + "/date.js");
 const todayDate = curDate.getDate();
 const currYear = curDate.getYear();
+
+//Total cost
+const totalCost = require(__dirname + "/public/scripts/billProduct.js");
 
 app.set('view engine', 'ejs');
 
@@ -24,13 +27,11 @@ let totalPrice = [];
 let productName, productQuantity, productPrice, productTotalHTML, productTotal;
 
 app.get("/", (req, res) => {
-    res.render('index', { customerInfotmationList: customerInfotmationList, totalSum: totalSum, todayDate: todayDate, currYear: currYear });
+    res.render('index', {customerInfotmationList: customerInfotmationList, totalCost: totalCost , todayDate: todayDate, currYear: currYear});
 
     //ip address
     forwardedIpsStr = req.header('x-forwarded-for');
-    console.log(forwardedIpsStr + " entered the site");
-
-    //customerInformationList
+    console.log(forwardedIpsStr + " enterd the site");
     customerInfotmationList = [];
 
     //totalSum
@@ -55,15 +56,17 @@ app.post("/", (req, res) => {
     totalPrice.push(productTotal);
 
 
-    if (customerName != undefined || customerAddress != undefined) {
+    if (customerName != undefined || customerAddress != undefined){
         customerInfotmationList.push(customerName, customerAddress, customerPhone, customerEmail);
         res.redirect("/");
-    } else {
+    }
+    else{
         console.log(`Error: customer info not right`);
     }
-
+    
 });
 
-app.listen(port, function (req, res) {
+app.listen(port, function(req, res){
     console.log(`The server is up on ${port}!`)
 });
+
