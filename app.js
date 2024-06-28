bdaef6c05ee2e08f28ce4f79192ad4cd58cc4e96
Ghassan Alnajjar <ghassan.f.najjar@gmail.com>
2024-06-27 19:09:51 -0600

    Database made and configured schema

--- app.js
+++ app.js
@@ -5,8 +5,56 @@ const bodyParser = require('body-parser');
 const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { unique } = require('webpack-merge');
 
 app.use(express.static("public"), bodyParser.urlencoded({ extended: true }));
+
+//mongodb data schema
+const dbURL = "mongodb+srv://ghassan77:" + process.env.DB_HOST_KEY + "@secrets.qzcaw0j.mongodb.net/bavariaDB";
+mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("Connected to database!");
+    })
+    .catch((err) => {
+        console.log("Error: " + err);
+    });
+
+//schema
+const userSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: false
+    },
+    address: String,
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    totalCost: String,
+    product: String,
+    quantity: Number,
+    price: Number,
+    date: {
+        type: String,
+        required: true,
+        unique: false
+    },
+});
+
+// making a model
+const Customer = new mongoose.model("Customer", userSchema);
 
 //date and time
 const curDate = require(__dirname + "/date.js");
@@ -29,14 +77,40 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
+    //customer info
     customerName = req.body.customerName;
     customerAddress = req.body.customerAddress;
     customerPhone = req.body.customerPhone;
     customerEmail = req.body.customerEmail;
     console.log(customerName);
 
+    //check if the customer info is right
     if (customerName != undefined || customerAddress != undefined){
         customerInfotmationList.push(customerName, customerAddress, customerPhone, customerEmail);
+
+        //Generate a random number for the invoice
+        const invoiceNumber = Math.floor(Math.random() * 1000000);
+
+        //save the customer info to the database
+        try{
+            const customerInfo = new Customer({
+                id: invoiceNumber,
+                name: customerName,
+                address: customerAddress,
+                phone: customerPhone,
+                email: customerEmail,
+                totalCost: "$NA",
+                date: todayDate
+            });
+            
+            //savedb
+            customerInfo.save();
+        }
+        catch(err){
+            console.log(`Error: ${err}`);
+        }
+        
+        //redirect to the home page
         res.redirect("/");
     }
     else{
